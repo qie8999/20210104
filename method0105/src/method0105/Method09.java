cb6dc0999e8148744d324b651ee4feafd9545dad
qie8999 <qie8999@naver.com>
2021-01-05 16:45:19 +0900

    0105 메소드연습 추가수정

--- method0105/src/method0105/Method09.java
+++ method0105/src/method0105/Method09.java
@@ -1,10 +1,25 @@
 package method0105;
 
+import java.util.Scanner;
+
 public class Method09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 09 3개의 숫자를 입력받아서 
+		//최대, 최소값 구하는 프로그램
+		Scanner s = new Scanner(System.in);
+		System.out.println("첫번째 숫자 ->");
+		int num1 = s.nextInt();
+		System.out.println("두번째 숫자 ->");
+		int num2 = s.nextInt();
+		System.out.println("세번째 숫자 ->");
+		int num3 = s.nextInt();
+		
 
+	}
+	public static void func1(int num1,int num2,int num3) {
+		int arr[]=new int[3];
+		
 	}
 
 }
